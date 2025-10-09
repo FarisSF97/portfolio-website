2d8309f557fb21018ddef70558808272da64d0f9
FarisSF97 <alfarisi97@outlook.com>
2025-10-09 08:39:02 +0700

    add gsap

---
+++ assets/js/main.js
@@ -0,0 +1,28 @@
+// MENU SHOW & HIDDEN
+
+// MENU SHOW
+// Validate if constant exists
+
+// MENU HIDDEN
+// Validate if constant exists
+
+// REMOVE MENU MOBILE
+
+// CHANGE BACKGROUND HEADER
+
+// SCROLL SECTIONS ACTIVE LINK
+
+// SCROLL ABOUT ANIMATION
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.class');
+
+// DARK LIGHT THEME
+
+// MIXITUP FILTER PORTFOLIO
+
+// Active work
+
+// EMAIL JS
+
+// SCROLL REVEAL ANIMATION
