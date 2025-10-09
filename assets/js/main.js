498f87df45070283cab5660d74ece6706102d46d
FarisSF97 <alfarisi97@outlook.com>
2025-10-09 09:29:25 +0700

    add scroll effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,7 +15,18 @@
 // SCROLL ABOUT ANIMATION
 gsap.registerPlugin(ScrollTrigger);
 
-gsap.utils.toArray('.class');
+gsap.utils.toArray('.text-gradient').forEach((span) => {
+  gsap.to(span, {
+    backgroundSize: '100% 100%',
+    ease: 'none',
+    scrollTrigger: {
+      trigger: span,
+      start: 'top bottom',
+      end: 'top center',
+      scrub: true,
+    },
+  });
+});
 
 // DARK LIGHT THEME
 
