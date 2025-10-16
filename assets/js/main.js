d8c444b253faa31df90f9293b86f0159556e3c56
FarisSF97 <alfarisi97@outlook.com>
2025-10-16 12:50:41 +0700

    make navbar fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,15 @@
 // REMOVE MENU MOBILE
 
 // CHANGE BACKGROUND HEADER
+const scrollHeader = () => {
+  const header = document.getElementById('header');
+
+  this.scrollY >= 20
+    ? header.classList.add('scroll-header')
+    : header.classList.remove('scroll-header');
+}
+
+window.addEventListener('scroll', scrollHeader);
 
 // SCROLL SECTIONS ACTIVE LINK
 
