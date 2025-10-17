fdcdc768ba7f204e48e371e3c0ad7c7dfed70a88
FarisSF97 <alfarisi97@outlook.com>
2025-10-18 05:37:51 +0700

    add responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,41 @@
 // MENU SHOW & HIDDEN
+const navMenu = document.getElementById('nav-menu'),
+  navToggle = document.getElementById('nav-toggle'),
+  navClose = document.getElementById('nav-close');
 
 // MENU SHOW
 // Validate if constant exists
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  });
+}
 
 // MENU HIDDEN
 // Validate if constant exists
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  });
+}
+
+document.addEventListener('click', function(event) {
+  const isClickInside = navMenu.contains(event.target) || (navToggle && navToggle.contains(event.target));
+  if (!isClickInside && navMenu.classList.contains('show-menu')) {
+    navMenu.classList.remove('show-menu');
+  }
+});
 
 // REMOVE MENU MOBILE
+const navLink = document.querySelectorAll('.nav-link');
+
+const linkAction = () => {
+  const navMenu = document.getElementById('nav-menu');
+
+  navMenu.classList.remove('show-menu');
+};
+
+navLink.forEach((n) => n.addEventListener('click', linkAction));
 
 // CHANGE BACKGROUND HEADER
 const scrollHeader = () => {
