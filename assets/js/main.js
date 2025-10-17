3f951d13eb7e7d47114c69e11da00c38f731e4e2
FarisSF97 <alfarisi97@outlook.com>
2025-10-17 10:54:26 +0700

    add light theme

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,6 +60,31 @@ gsap.utils.toArray('.text-gradient').forEach((span) => {
 });
 
 // DARK LIGHT THEME
+window.addEventListener('DOMContentLoaded', () => {
+  const toggleBtn = document.getElementById('theme-toggle');
+
+  function applyTheme(theme) {
+    if (theme === 'light') {
+      document.body.classList.add('light-theme');
+      toggleBtn.classList.remove('ri-sun-line');
+      toggleBtn.classList.add('ri-moon-line');
+    } else {
+      document.body.classList.remove('light-theme');
+      toggleBtn.classList.add('ri-sun-line');
+      toggleBtn.classList.remove('ri-moon-line');
+    }
+
+    localStorage.setItem('theme', theme);
+  }
+
+  const savedTheme = localStorage.getItem('theme') || 'dark';
+  applyTheme(savedTheme);
+
+  toggleBtn.addEventListener('click', () => {
+    const isLight = document.body.classList.contains('light-theme');
+    applyTheme(isLight ? 'dark' : 'light');
+  });
+});
 
 // MIXITUP FILTER PORTFOLIO
 var mixer = mixitup('.work-container', {
